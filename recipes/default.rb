8a071b1c8842f7476ac8665f9057f7d786c4c20e
adam_w_chen <adam_w_chen@trend.com.tw>
2016-04-30 19:38:55 +0800

    final example and recipe

--- recipes/default.rb
+++ recipes/default.rb
@@ -21,54 +21,41 @@ users.each do |login|
     supports  :manage_home => true
   end
   
-        directory "#{home}/.ssh" do
-          owner user['uid'] ? validate_id(user['uid']) : user['username']
-          group validate_id(user['gid']) if user['gid']
-          mode '0700'
-          only_if { user['ssh_keys'] || user['ssh_private_key'] || user['ssh_public_key'] }
-        end
+  directory "#{home}/.ssh" do
+    owner user['uid'] ? validate_id(user['uid']) : user['username']
+    group validate_id(user['gid']) if user['gid']
+    mode '0700'
+    only_if { user['ssh_keys'] || user['ssh_private_key'] || user['ssh_public_key'] }
+  end
 
-        template "#{home}/.ssh/authorized_keys" do
-          source 'authorized_keys.erb'
-          owner user['uid'] ? validate_id(user['uid']) : user['username']
-          group validate_id(user['gid']) if user['gid']
-          mode '0600'
-          variables ssh_keys: user['ssh_keys']
-          only_if { user['ssh_keys'] }
-        end
+  template "#{home}/.ssh/authorized_keys" do
+    source 'authorized_keys.erb'
+    owner user['uid'] ? validate_id(user['uid']) : user['username']
+    group validate_id(user['gid']) if user['gid']
+    mode '0600'
+    variables ssh_keys: user['ssh_keys']
+    only_if { user['ssh_keys'] }
+  end
 
-        if user['ssh_private_key']
-          key_type = user['ssh_private_key'].include?('BEGIN RSA PRIVATE KEY') ? 'rsa' : 'dsa'
-          template "#{home}/.ssh/id_#{key_type}" do
-            source 'private_key.erb'
-            owner user['uid'] ? validate_id(user['uid']) : user['username']
-            group validate_id(user['gid']) if user['gid']
-            mode '0400'
-            variables private_key: user['ssh_private_key']
-          end
-        end
+  if user['ssh_private_key']
+    key_type = user['ssh_private_key'].include?('BEGIN RSA PRIVATE KEY') ? 'rsa' : 'dsa'
+    template "#{home}/.ssh/id_#{key_type}" do
+      source 'private_key.erb'
+      owner user['uid'] ? validate_id(user['uid']) : user['username']
+      group validate_id(user['gid']) if user['gid']
+      mode '0400'
+      variables private_key: user['ssh_private_key']
+    end
+  end
 
-        if user['ssh_public_key']
-          key_type = user['ssh_public_key'].include?('ssh-rsa') ? 'rsa' : 'dsa'
-          template "#{home}/.ssh/id_#{key_type}.pub" do
-            source 'public_key.pub.erb'
-            owner user['uid'] ? validate_id(user['uid']) : user['username']
-            group validate_id(user['gid']) if user['gid']
-            mode '0400'
-            variables public_key: user['ssh_public_key']
-          end
-        end
-
-end
-
-user 'testing' do
-  comment 'A random user'
-  home '/home/testing'
-  shell '/bin/bash'
-  password '$1$JJsvHslasdfjVEroftprNn4JHtDi'
-end
-
-users_manage 'testgroup2' do
-  action [:create]
-  data_bag 'users'
+  if user['ssh_public_key']
+    key_type = user['ssh_public_key'].include?('ssh-rsa') ? 'rsa' : 'dsa'
+    template "#{home}/.ssh/id_#{key_type}.pub" do
+      source 'public_key.pub.erb'
+      owner user['uid'] ? validate_id(user['uid']) : user['username']
+      group validate_id(user['gid']) if user['gid']
+      mode '0400'
+      variables public_key: user['ssh_public_key']
+    end
+  end
 end
